c5d68ab4a830bc3d224412d4eef13d21d07fef28
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-06-19 22:26:38 -0500

    Add test

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Book
 
-# Create your tests here.
+class BookModelTest(TestCase):
+    def test_create_book(self):
+        book = Book.objects.create(
+            title="Clean Code",
+            author="Robert C. Martin",
+            published="2008-08-01"
+        )
+        self.assertEqual(book.title, "Clean Code")
+        self.assertEqual(str(book), "Clean Code")
