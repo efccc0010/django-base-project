9df02496545d5adaf80278d554d4cd213e4546e9
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-06-20 23:24:20 -0500

    Fix

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,18 @@
-# ✅ CORRECTO
 from django.urls import path
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+router = DefaultRouter()
+router.register(r'books', BookViewSet, basename='book')
+
 urlpatterns = [
+    # JWT endpoints
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+urlpatterns += router.urls
