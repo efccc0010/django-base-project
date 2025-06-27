746ba54b080c602e4baec756793ac6d8dd19d666
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-06-27 00:35:07 -0500

    Fix

--- core/views.py
+++ core/views.py
@@ -8,4 +8,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [AllowAny]  # [IsAuthenticated]
