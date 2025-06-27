68ae1f8acc0561e14f34be994e226dbc2803031f
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-06-27 00:17:04 -0500

    Fix

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView  # noqa
 
 from .views import BookViewSet
 
