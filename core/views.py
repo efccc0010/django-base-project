f1a70737f0f7feb02c6c132070c0109de63253ac
efccc0010 <edgarfernandocarrionccoicca@gmail.com>
2025-06-27 00:34:46 -0500

    Temporaty alloany

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from .models import Book
 from .serializers import BookSerializer
@@ -8,4 +8,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
